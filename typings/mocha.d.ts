c21373b0996665d43129a8aad0b51180b97e73b7
wwestrop <wwestrop@hotmail.com>
2018-04-12 19:56:42 +0100

    Typings

--- typings/mocha.d.ts
+++ typings/mocha.d.ts
@@ -1,4 +1,18 @@
+
+//// <reference types="Mocha" />
+
+//// <reference types="node" />
+
 declare namespace Mocha {
-    interface IRunner extends NodeJS.EventEmitter {
-    }
+   interface IRunner extends NodeJS.EventEmitter {
+   }
+
+   interface ISuite extends NodeJS.EventEmitter {
+       // These properties not documented as public or private (only the methods have this documentation)
+       // Thus, not sharing these publically in case they break
+       suites: ISuite[];
+       tests: Mocha.ITest[];
+       pending: boolean;
+       root: boolean;
+   }
 }
