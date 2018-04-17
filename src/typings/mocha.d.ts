29d5ffeda67fab207e323594b11a2bc3f0aa4b65
wwestrop <wwestrop@hotmail.com>
2018-04-17 22:25:09 +0100

    Merged typings

--- src/typings/mocha.d.ts
+++ src/typings/mocha.d.ts
@@ -5,7 +5,19 @@ declare module "mocha" {
 		total: number;
 	}
 
-	 export interface ITest {
+	export interface ITest {
+		duration: number;
+		file?: string;
+	}
+
+	export interface ISuite extends NodeJS.EventEmitter {
+		// These properties not documented as public or private (only the methods have this documentation)
+		// Thus, not sharing these publically in case they break
+		suites: ISuite[];
+		tests: ITest[];
+		pending: boolean;
+		root: boolean;
+ 
 		duration: number;
 		file?: string;
 	}
