b3a92bc65e08b63655a957e4ba8295195d0afcc0
Will Westrop <wwestrop@hotmail.com>
2019-01-20 12:55:45 +0000

    Tests

--- test/logic/RunnerFactory.test.ts
+++ test/logic/RunnerFactory.test.ts
@@ -1,5 +1,44 @@
+import { RunnerFactory } from "../../src/ts/logic/RunnerFactory";
+import { ITestAdapter } from '../../src/ts/logic/ITestAdapter';
+import { mock, when, anything, instance } from "ts-mockito";
+import { TestsNotDiscoveredException } from "../../src/ts/exceptions/TestsNotDiscoveredException";
+import { Directory } from "../../lib/LibFs/Fs";
+import { MochaTestAdapter } from "../../src/ts/mocha/MochaTestAdapter";
+import { LitmusContext } from "../../src/ts/types/LitmusContext";
+import { ITestRunner } from "../../src/ts/logic/ITestRunner";
+import { throws } from "assert";
+import { assert } from 'chai';
+
+
+// TODO ts-mokito wouldn't let me mock an interface ITestAdapter
+abstract class MyAdapter implements ITestAdapter {
+
+	isCompatible(directory: Directory, ctxt?: LitmusContext): boolean {
+		throw new Error("Method not implemented.");
+	}
+
+	buildTestRunner(directory: Directory, ctxt?: LitmusContext): ITestRunner {
+		throw new Error("Method not implemented.");
+	}
+}
 
 describe("RunnerFactory", () => {
+
+	let incompatibleRunner: ITestAdapter;
+	let compatibleRunner: ITestAdapter;
+
+	beforeEach(() => {
+
+		incompatibleRunner = mock(MyAdapter);
+		compatibleRunner = mock(MyAdapter);
+
+		when(incompatibleRunner.isCompatible(anything(), anything())).thenReturn(false);
+		when(compatibleRunner.isCompatible(anything(), anything())).thenReturn(true);
+
+		incompatibleRunner = instance(incompatibleRunner);
+		compatibleRunner = instance(compatibleRunner);
+	});
+
 
 	it("should have some tests", () => {
 		throw "It doesn't have any tests";
@@ -14,7 +53,20 @@ describe("RunnerFactory", () => {
 	});
 
 	it("should throw if no compatible frameworks found", () => {
-		throw "It doesn't have any tests";
+		const dir = new Directory("");
+
+		const sut =  new RunnerFactory([incompatibleRunner]);
+		try {
+			sut.build(dir);
+		}
+		catch (ex) {
+			if (ex instanceof TestsNotDiscoveredException) {
+				// Success
+				return;
+			}
+		}
+
+		assert.fail();
 	});
 
 });
